6320f84a80b93450f49987300e96028ac374cf80
lewis nyoike <nyoikelewis@gmail.com>
2017-05-30 12:10:10 +0300

    working now

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
-@Component ({
+@Component({
   selector: 'my-app',
   template: `
+  <div class="container">
   <h1>My First Angular 2 App</h1>
+  </div>
   `
 })
 
-export class AppComponent{
-
+export class AppComponent {
 }
